ba7684d27f2de1b677c69f76218691fb81428aa0
gonzodevs <Nico@Nicolass-Mac-Pro.local>
2014-10-23 00:28:19 -0400

    Checking 2(partial)
    on RedBlackTree:
    Implemented find,and insert.
    toString and afterInsertion still need work.

--- src/RedBlackNode.java
+++ src/RedBlackNode.java
@@ -2,10 +2,13 @@
  * Created by n_gonzal on 10/9/2014.
  */
 public class RedBlackNode {
+    //given
     private Element element;
     private RedBlackNode left;
     private RedBlackNode right;
     private int color;
+    //added
+
 
     public RedBlackNode(Element e, RedBlackNode L, RedBlackNode r){
         element =e;
@@ -13,7 +16,20 @@ public class RedBlackNode {
         right=r;
         color=0;
     }
-
+    //added default constructor;
+    public RedBlackNode(){
+        element =null;
+        left=null;
+        right=null;
+        color=0;
+    }
+    //added modified constructor
+    public RedBlackNode(Element e){
+        element =e;
+        left=null;
+        right=null;
+        color=0;
+    }
     public Element getElement(){
         return element;
     }
@@ -23,7 +39,21 @@ public class RedBlackNode {
     public RedBlackNode getLeftChild(){
         //to do
     }
-    public RedBlackNode getRigthChild(){
+    public RedBlackNode getRightChild(){
         //to do
+    }
+    //All methods following this comment have been added additionally to original instructions.
+    //added set element for node
+    public void SetElement(int k, String v){
+        element=new Element(k,v);
+    }
+    public void setLeftChild(RedBlackNode n){
+        left=n;
+    }
+    public void setRightChild(RedBlackNode n){
+        right=n;
+    }
+    public void setColor(int a){
+        color=a;
     }
 }
--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -3,30 +3,116 @@
  */
 public class RedBlackTree implements TreeInterface {
 
+    RedBlackNode root;
+    RedBlackNode t=new RedBlackNode();
     @java.lang.Override
     public String find(int key) {
-        //Same as regular BRT
+        //key has matched a key on the tree
+        if(t.getElement().getKey()==key)
+            return t.getElement().getValue();
+        //looks at left subtree
+        if(t.getElement().getKey()< key)
+        {
+            return find(t.getLeftChild().getElement().getKey());
+        }
+        //look at right subtree
+        if(t.getElement().getKey()> key)
+        {
+            return find(t.getRightChild().getElement().getKey());
+        }
 
         return null;
     }
 
-    @java.lang.Override
+
     public void insert(int key, String value) {
 
+        if(root==null) {
+            root = new RedBlackNode(new Element(key,value));
+        }
+        RedBlackNode t=root;
+        while (true) {
+            int Comparison = compare(key,t.getElement().getKey());
+            if (Comparison == 0) {
+                t.SetElement(key, value);
+            } else if (Comparison < 0) {
+                if (t.getLeftChild() == null) {
+                    t.setLeftChild(new RedBlackNode(new Element(key,value)));
+                    afterInsertion(t.getLeftChild());
+                    break;
+                }
+                t = t.getLeftChild();
+            } else {
+                if (t.getRightChild() == null) {
+                    t.setRightChild(new RedBlackNode(new Element(key,value)));
+                    afterInsertion((Node) n.getRight());
+                    break;
+                }
+                t = t.getRightChild();
+            }
+        }
     }
 
-    @java.lang.Override
+
     public void delete(int key) {
-
+        throw new UnsupportedOperationException( );
     }
 
-    @java.lang.Override
+
     public String remove(int key) {
-        return null;
+        throw new UnsupportedOperationException( );
     }
 
     @java.lang.Override
-    public String toString(object a){
+    public String toString(){
+        throw new UnsupportedOperationException( );
 
     }
+
+    //All methods after this comment have been added
+    public void afterInsertion(RedBlackNode t){
+        if (n != null && n != root && isRed(parentOf(n))) {
+
+            // Step 2a (simplest): Recolor, and move up to see if more work
+            // needed
+            if (isRed(siblingOf(parentOf(n)))) {
+                setColor(parentOf(n), Color.black);
+                setColor(siblingOf(parentOf(n)), Color.black);
+                setColor(grandparentOf(n), Color.red);
+                adjustAfterInsertion(grandparentOf(n));
+            }
+
+            // Step 2b: Restructure for a parent who is the left child of the
+            // grandparent. This will require a single right rotation if n is
+            // also
+            // a left child, or a left-right rotation otherwise.
+            else if (parentOf(n) == leftOf(grandparentOf(n))) {
+                if (n == rightOf(parentOf(n))) {
+                    rotateLeft(n = parentOf(n));
+                }
+                setColor(parentOf(n), Color.black);
+                setColor(grandparentOf(n), Color.red);
+                rotateRight(grandparentOf(n));
+            }
+
+            // Step 2c: Restructure for a parent who is the right child of the
+            // grandparent. This will require a single left rotation if n is
+            // also
+            // a right child, or a right-left rotation otherwise.
+            else if (parentOf(n) == rightOf(grandparentOf(n))) {
+                if (n == leftOf(parentOf(n))) {
+                    rotateRight(n = parentOf(n));
+                }
+                setColor(parentOf(n), Color.black);
+                setColor(grandparentOf(n), Color.red);
+                rotateLeft(grandparentOf(n));
+            }
+        }
+
+        // Step 3: Color the root black
+        setColor((Node) root, Color.black);
+    }
+
+    }
+
 }
